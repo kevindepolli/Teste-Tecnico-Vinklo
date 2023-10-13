2957ac5c55b4189cf6d3b5490ee6cdda65ca9a94
Kevin Depolli <kevindepollidcf@gmail.com>
2023-10-13 14:38:41 -0300

    Adicionar Validações
    Foram adicionadas validações para todos os campos de User.
    
    - adicionar dependencia cpf cnpj;
    - validações do model de user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+require "cpf_cnpj"
 class User < ApplicationRecord
+  validates :name, :phone, :email, :cpf, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, format: { with:  /\s*(\d{2}|\d{0})[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. ]?\s*/ }
+  validate :cpf_valid?
+
+  def cpf_valid?
+    return if CPF.valid?(cpf)
+
+    errors.add(:cpf)
+  end
 end
